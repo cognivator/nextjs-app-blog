66dce1dbad71c6494e0fc26f2910e5994b96e8c0
cognivator (slhenty) <slhenty@cognivator.com>
2023-10-05 19:52:31 -0400

    feat (page): dashboard page and layout

---
+++ app/(app)/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+/**
+ * Created by slhenty on 2023-10-5.
+ */
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+}
+
+export default function RootLayout({children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>{children}</>
+  )
+}
---
+++ app/(app)/dashboard/page.jsx
@@ -0,0 +1,6 @@
+/**
+ * Created by slhenty on 2023-10-5.
+ */
+export default function Page() {
+  return <h2>Hello, Dashboard!</h2>
+}
